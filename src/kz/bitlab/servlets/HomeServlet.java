00fecde9f6ce20871a1c4c90fe82aa3bbe6ca4f2
Aidar <bai.aidar.kz@gmail.com>
2022-07-15 14:50:31 +0600

    Task 1-1

---
+++ src/kz/bitlab/servlets/HomeServlet.java
@@ -0,0 +1,20 @@
+package kz.bitlab.servlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(value = "/")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        for (int i = 0; i < 10; i++) {
+            out.print("<h1>HELLO BITLAB</h1>");
+        }
+    }
+}
